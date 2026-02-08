4ca33e1809db8a2adf9e16b5513e2f313d4a3145
Fangxingzonghuofan <2457785778@qq.com>
2026-02-08 16:01:23 +0800

    我写的并非空间上的最优解法，但胜在简单，学会了calloc

---
+++ 矩阵置零.c
@@ -0,0 +1,26 @@
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
+    int rows[matrixSize];
+    int cols[matrixColSize[0]];
+    for (int i = 0; i < matrixSize; i++)
+    {
+        for (int j = 0; j < *matrixColSize; j++)
+        {
+            if (matrix[i][j]==0)
+            {
+                rows[i]=1;
+                cols[j]=1;
+            }
+        }
+    }
+    for (int i = 0; i < matrixSize; i++)
+    {
+        for (int j = 0; j <*matrixColSize; j++)
+        {
+            if (rows[i]==1||cols[j]==1)
+            {
+                matrix[i][j]=0;
+            }
+        }
+    }
+    return;
+}
