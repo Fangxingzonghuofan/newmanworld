337062a8dffdbd500b35a568bd9a218944dce3ee
Fangxingzonghuofan <2457785778@qq.com>
2026-03-04 21:44:11 +0800

    简单题，简单方法简单的要命，难的方法难得入土，没错说的就是你：根据完全二叉树的性质使用二分查找➕位运算的方法！

---
+++ 完全二叉树的节点个数.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <sstream>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+int num=0;
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        num++;
+        countNodes(root->left);
+        countNodes(root->right);
+        return num;
+    }
+};//最简单最容易想到的方法，现在考虑利用完全二叉树的性质来解题,太难了放弃，贴出使用二分查找的方法供参考
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode* node = root;
+        while (node->left != nullptr) {
+            level++;
+            node = node->left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    bool exists(TreeNode* root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode* node = root;
+        while (node != nullptr && bits > 0) {
+            if (!(bits & k)) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+            bits >>= 1;
+        }
+        return node != nullptr;
+    }
+};
