2ea38c054d93c14a39e782c21ea5613249dfbf43
Fangxingzonghuofan <2457785778@qq.com>
2026-03-20 15:23:43 +0800

    很普遍的二叉搜索树递归写法，不同的是要求平衡只需要以中间的值为根节点递归就好，简单

---
+++ 将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <sstream>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+    TreeNode*build(vector<int>& nums,int left,int right){
+        if(left>right) return nullptr;
+        int mid=left+(right-left)/2;
+        TreeNode *root=new TreeNode(nums[mid]);
+        root->left=build(nums,left,mid-1);
+        root->right=build(nums,mid+1,right);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return build(nums,0,nums.size()-1);
+    }
+};
