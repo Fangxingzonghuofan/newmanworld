5fc047b86c6d3e0167bb22985f7d3f83e228e0b9
ZhuQiwen <2457785778@qq.com>
2026-05-13 16:40:43 +0800

    二刷，补充了一个c++版本，移动左右两边较小的那一个指针即可，特别注意相等时移动哪一边都可以
    Added C++ version of maxArea function with comments.

--- 盛最多水的容器.c
+++ 盛最多水的容器.c
@@ -20,3 +20,26 @@ int maxArea(int* height, int heightSize) {
     }
     return Maxsize;
 }
+//二刷，补充c++版本
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=height.size();
+        int low=0,high=l-1;
+        int capa=0;
+        int maxCapability=capa;
+        while(low<high){
+            capa=min(height[low],height[high])*(high-low);
+            maxCapability=maxCapability>capa?maxCapability:capa;
+            if(height[low]<height[high]) ++low;
+            else --high;
+        }/*注意：当height[low]==height[high]时
+        ++low和--right都可以
+        这是因为，无论哪一种选择，改变后的capa一定小于之前的capa
+        要对结果产生影响的话（新的capa大于max或者旧的capa），一定是
+        中间出现了两个都大于height[low]和height[high]的数，
+        因此移动哪一边的指针都没关系
+        */
+        return maxCapability;
+    }
+};
